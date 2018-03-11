f78e762751846363229553d0398539e6d5a3e0b9
Mikko - C2Native <ainia@c2native.io>
2018-03-11 12:50:32 +0400

    front-end header and footer

--- js/canvas.js
+++ js/canvas.js
@@ -1,8 +1,8 @@
-var stage = new createjs.Stage("volentix-bg");
-var circle = new createjs.Bitmap("library/src/assets/volentix_wheel_1.svg");
-circle.rotation
-stage.addChild(circle);
-
-$(function(){
-  stage.update();
-});
+// var stage = new createjs.Stage("volentix-bg");
+// var circle = new createjs.Bitmap("library/src/assets/volentix_wheel_1.svg");
+// circle.rotation
+// stage.addChild(circle);
+//
+// $(function(){
+//   stage.update();
+// });
--- js/custom.js
+++ js/custom.js
@@ -11,34 +11,36 @@
 //   }
 // });
 
-$('#vtx-wheel .wheel-dot').each(function(){
-  $(this).click(function(){
-    $node = '#' + $(this).attr('mydata:node');
-    $('#vtx-wheel .wheel-dot').children('circle').css('fill', '#ffffff');
-    $(this).children('circle').css('fill', '#cf7532');
 
-    $('#vtx-text .nodes').removeClass('active').css('fill', '#ffffff');
-    $($node).toggleClass('active');
+$(function(){
+  $('#vtx-wheel .wheel-dot').each(function(){
+    $(this).click(function(){
+      $node = '#' + $(this).attr('mydata:node');
+      $('#vtx-wheel .wheel-dot').children('circle').css('fill', '#ffffff');
+      $(this).children('circle').css('fill', '#cf7532');
+
+      $('.nodes').removeClass('active').css('fill', '#ffffff');
+      $($node).toggleClass('active');
+
+      function typeWriter(el, text, n) {
+        if (n < (text.length)) {
+          $(el).html(text.substring(0, n+1));
+          n++;
+          setTimeout(function() {
+            typeWriter(el, text, n)
+          }, 25);
+        }
+      }
+
+      var el = $($node).find('p');
+      var text = el.data('text');
+      typeWriter(el, text, 0);
+    });
   });
 });
-  // var svgCircle = $(this).children('circle');
-  //
-  // var wheelDot = anime({
-  //   targets: svgCircle,
-  //   fill: '#cf7532',
-  //   easing: 'easeInOutSine',
-  //   autoplay: false
-  // });
-  //
-  // new Vue({
-  //   el: this,
-  //   methods:  {
-  //     animateWheelDot: function() {
-  //       wheelDot.reset();
-  //       wheelDot.play();
-  //     }
-  //   }
-  // });
+
+
+
 
 
 // document.getElementById('vtx-wheel').function(){
@@ -95,23 +97,23 @@ function init() {
   camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
   camera.position.z = 1000;
   scene = new THREE.Scene();
-  particles = new Array();
-  var PI2 = Math.PI * 2;
-  var material = new THREE.LineBasicMaterial({ color: 0xffffff });
-
-  var geometry = new THREE.Geometry();
-  geometry.vertices.push(new THREE.Vector3(-10, 0, 0));
-  geometry.vertices.push(new THREE.Vector3(20, 10, 0));
-
-  var i = 0;
-  for ( var ix = 0; ix < AMOUNTX; ix ++ ) {
-    for ( var iy = 0; iy < AMOUNTY; iy ++ ) {
-      particle = particles[ i ++ ] = new THREE.Line(geometry, material);
-      particle.position.x = ix * SEPARATION - ( ( AMOUNTX * SEPARATION ) / 2 );
-      particle.position.z = iy * SEPARATION - ( ( AMOUNTY * SEPARATION ) / 2 );
-      scene.add( particle );
-    }
-  }
+  // particles = new Array();
+  // var PI2 = Math.PI * 2;
+  // var material = new THREE.LineBasicMaterial({ color: 0xffffff });
+  //
+  // var geometry = new THREE.Geometry();
+  // geometry.vertices.push(new THREE.Vector3(-10, 0, 0));
+  // geometry.vertices.push(new THREE.Vector3(20, 10, 0));
+  //
+  // var i = 0;
+  // for ( var ix = 0; ix < AMOUNTX; ix ++ ) {
+  //   for ( var iy = 0; iy < AMOUNTY; iy ++ ) {
+  //     particle = particles[ i ++ ] = new THREE.Line(geometry, material);
+  //     particle.position.x = ix * SEPARATION - ( ( AMOUNTX * SEPARATION ) / 2 );
+  //     particle.position.z = iy * SEPARATION - ( ( AMOUNTY * SEPARATION ) / 2 );
+  //     scene.add( particle );
+  //   }
+  // }
 
   planet1 = new THREE.Group();
 	scene.add( planet1 );
@@ -140,17 +142,17 @@ function onWindowResize() {
   renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
-function updatePlane() {
- for (var i = 0; i < plane.geometry.vertices.length; i++)
- {
-   plane.geometry.vertices[i].z += Math.random()*vertexHeight     -vertexHeight;
- }
-};
-
-function animateShit(){
- plane.geometry.vertices[1].z+=.01;
- plane.geometry.verticesNeedUpdate=true;
-}
+// function updatePlane() {
+//  for (var i = 0; i < plane.geometry.vertices.length; i++)
+//  {
+//    plane.geometry.vertices[i].z += Math.random()*vertexHeight     -vertexHeight;
+//  }
+// };
+//
+// function animateShit(){
+//  plane.geometry.vertices[1].z+=.01;
+//  plane.geometry.verticesNeedUpdate=true;
+// }
 //
 function animate() {
   requestAnimationFrame( animate );
@@ -165,22 +167,22 @@ function render() {
   camera.position.z = 1000;
   camera.lookAt( scene.position );
 
-  var i = 0;
-  for ( var ix = 0; ix < AMOUNTX; ix ++ ) {
-    for ( var iy = 0; iy < AMOUNTY; iy ++ ) {
-      particle = particles[ i++ ];
-      particle.position.y = ( Math.sin( ( ix + count ) * 0.3 ) * 50 ) +
-        ( Math.sin( ( iy + count ) * 0.5 ) * 50 );
-    }
-  }
+  // var i = 0;
+  // for ( var ix = 0; ix < AMOUNTX; ix ++ ) {
+  //   for ( var iy = 0; iy < AMOUNTY; iy ++ ) {
+  //     particle = particles[ i++ ];
+  //     particle.position.y = ( Math.sin( ( ix + count ) * 0.3 ) * 50 ) +
+  //       ( Math.sin( ( iy + count ) * 0.5 ) * 50 );
+  //   }
+  // }
 
   planet1.position.z = -2000;
   planet1.position.x = 500;
 
   scene.traverse( function( planet1 ) {
 		if ( planet1.isMesh === true ) {
-			planet1.rotation.x -= 0.001;
-			planet1.rotation.y -= 0.004;
+			planet1.rotation.x -= 0.0001;
+			planet1.rotation.y -= 0.0004;
 		}
 	} );
   renderer.render( scene, camera );
